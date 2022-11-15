2c4a44e5b40c9bd4a0d230212582f291f5e223ef
izuru <s0608sq_uell@docomo.ne.jp>
2022-11-15 13:48:31 +0900

    schema

---
+++ schema.sql
@@ -0,0 +1,24 @@
+SET SESSION FOREIGN_KEY_CHECKS=0;
+
+/* Drop Tables */
+
+DROP TABLE IF EXISTS quiz;
+
+
+
+
+/* Create Tables */
+
+CREATE TABLE quiz
+(
+	-- タスクID／主キー
+	id int NOT NULL AUTO_INCREMENT COMMENT 'タスクID／主キー',
+	-- 問題の内容
+	content text NOT NULL COMMENT '問題の内容',
+	-- 問題の答え
+	answer int  COMMENT '問題の答え',
+	PRIMARY KEY (id)
+);
+
+
+
